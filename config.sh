e2b6f2b75f08dfab3de0ac9a0598011ee55a0a9e
shackofnoreturn <shackofnoreturn@gmail.com>
2026-06-09 20:46:18 +0200

    Config refactor

--- config.sh
+++ config.sh
@@ -7,32 +7,6 @@
 # Includes
 source "$(dirname "$0")/lib/ui.sh"
 
-# Target Device Helper Function
-select_device() {
-
-    mapfile -t DEVICES < <(
-        lsblk -dpno NAME,SIZE,MODEL,TRAN |
-        grep -E 'usb|mmc'
-    )
-
-    MENU_ITEMS=()
-
-    for DEV in "${DEVICES[@]}"; do
-        NAME=$(echo "$DEV" | awk '{print $1}')
-        DESC=$(echo "$DEV" | cut -d' ' -f2-)
-
-        MENU_ITEMS+=("$NAME" "$DESC")
-    done
-
-    DEVICE=$(dialog \
-        --backtitle "$BACKTITLE" \
-        --title "Select SD Card" \
-        --menu "Choose target device" \
-        20 100 10 \
-        "${MENU_ITEMS[@]}" \
-        3>&1 1>&2 2>&3)
-}
-
 # Configuration file path
 CONFIG_FILE="$(dirname "$0")/config.env"
 
@@ -41,11 +15,7 @@ source "$CONFIG_FILE"
 
 # Configuration menu loop
 while true; do
-CHOICE=$(dialog \
-    --backtitle "$BACKTITLE" \
-    --title "Configuration" \
-    --menu "Select setting" \
-    20 70 10 \
+CHOICE=$(menu "Configuration" "Select setting to edit" \
     1 "Hostname: $HOSTNAME" \
     2 "Target Device: $DEVICE" \
     3 "IP Address: $IP_ADDRESS" \
@@ -53,11 +23,10 @@ CHOICE=$(dialog \
     5 "DNS Servers: $DNS_SERVERS" \
     6 "Username: $USERNAME" \
     7 "Password: $PASSWORD" \
-    8 "Save and Return" \
-    3>&1 1>&2 2>&3)
-RET=$?
+    8 "Save and Return")
 
 # Check if the user pressed Cancel or closed the dialog
+RET=$?
 if [ $RET -ne 0 ]; then
     break
 fi
@@ -65,11 +34,7 @@ fi
 # Handle menu choices
 case $CHOICE in
 1)
-HOSTNAME=$(dialog \
-    --backtitle "$BACKTITLE" \
-    --inputbox "Hostname" \
-    8 60 "$HOSTNAME" \
-    3>&1 1>&2 2>&3)
+HOSTNAME=$(input "Hostname" "Enter the hostname for the Raspberry Pi" "$HOSTNAME")
 ;;
 
 2)
@@ -77,43 +42,23 @@ select_device
 ;;
 
 3)
-IP_ADDRESS=$(dialog \
-    --backtitle "$BACKTITLE" \
-    --inputbox "IP Address" \
-    8 60 "$IP_ADDRESS" \
-    3>&1 1>&2 2>&3)
+IP_ADDRESS=$(input "IP Address" "Enter the static IP address for the Raspberry Pi" "$IP_ADDRESS")
 ;;
 
 4)
-GATEWAY=$(dialog \
-    --backtitle "$BACKTITLE" \
-    --inputbox "Gateway" \
-    8 60 "$GATEWAY" \
-    3>&1 1>&2 2>&3)
+GATEWAY=$(input "Gateway" "Enter the gateway IP address for the Raspberry Pi" "$GATEWAY")
 ;;
 
 5)
-DNS_SERVERS=$(dialog \
-    --backtitle "$BACKTITLE" \
-    --inputbox "DNS Servers" \
-    8 60 "$DNS_SERVERS" \
-    3>&1 1>&2 2>&3)
+DNS_SERVERS=$(input "DNS Servers" "Enter comma-separated DNS servers for the Raspberry Pi" "$DNS_SERVERS")
 ;;
 
 6)
-USERNAME=$(dialog \
-    --backtitle "$BACKTITLE" \
-    --inputbox "Username" \
-    8 60 "$USERNAME" \
-    3>&1 1>&2 2>&3)
+USERNAME=$(input "Username" "Enter the username for the Raspberry Pi" "$USERNAME")
 ;;
 
 7)
-PASSWORD=$(dialog \
-    --backtitle "$BACKTITLE" \
-    --inputbox "Password" \
-    8 60 "$PASSWORD" \
-    3>&1 1>&2 2>&3)
+PASSWORD=$(input "Password" "Enter the password for the Raspberry Pi" "$PASSWORD")
 ;;
 
 8)
